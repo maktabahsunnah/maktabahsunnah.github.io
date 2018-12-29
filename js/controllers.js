34e16fbbc17981dbeaf8c25e24288dd9310d7b47
maktabahsunnah <45748391+maktabahsunnah@users.noreply.github.com>
2018-12-30 00:02:24 +0700

    Create controllers.js

---
+++ js/controllers.js
@@ -0,0 +1,30 @@
+/*!
+ * ng-flowers v1.0.0 (http://elmahdim.com/demo/Angular-Flower-Expert-App)
+ * 
+ * Licensed under MIT ( link )
+ */
+ 
+"use strict";
+ 
+(function(){
+	var app = angular.module('scholarCtrls', []);
+	  
+     app.controller('scholarListCtrl', ['$http', '$scope', function($http, $scope){
+        $scope.scholars = [];
+
+        $http.get('data/scholars.json').success(function(data) {
+            $scope.scholars = data;
+        });
+		
+		 $scope.totalScholars = function(){
+			return $scope.scholars.length;
+		};
+    }]);
+	
+    app.controller('scholarDetailCtrl', ['$http', '$scope', '$routeParams', function($http, $scope,$routeParams){
+        $scope.scholars = [];
+        $http.get('data/' + $routeParams.flowerId + '.json').success(function(data) { 
+            $scope.scholars = data;
+        });
+    }]); 
+})();
