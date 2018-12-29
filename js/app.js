39a522219bc78a31bc2b9980d2b46772c8b517f5
maktabahsunnah <45748391+maktabahsunnah@users.noreply.github.com>
2018-12-30 00:00:35 +0700

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,46 @@
+/*!
+ * ng-flowers v1.0.0 (http://elmahdim.com/demo/Angular-Flower-Expert-App)
+ * 
+ * Licensed under MIT ( link )
+ */
+
+"use strict";
+ 
+(function(){
+	var app = angular.module('scholarApp', []);
+	
+	var app = angular.module('scholarApp', [
+	  'ngRoute'
+	  , 'ngSanitize'
+	  ,'scholarCtrls'
+	  ,'scholarDirectives'
+	]);
+	
+	app.config(['$routeProvider',
+	  function($routeProvider) {
+		$routeProvider
+		.when('/home', {
+			title: 'Maktabah As-Sunnah'
+			,templateUrl: 'partials/main.html'
+		  })
+		.when('/scholars', {
+			title: 'Scholars List'
+			,templateUrl: 'partials/scholar-list.html'
+			,controller : 'scholarListCtrl'
+		  })
+		.when('/scholars/:scholarId', {
+			templateUrl: 'partials/scholar-detail.html'
+			,controller : 'scholarDetailCtrl'
+		  })
+		.otherwise({
+			redirectTo: '/home'
+		  });
+	  }]);
+
+	app.run(['$location', '$rootScope', function($location, $rootScope) {
+		$rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+			$rootScope.title = current.$$route.title;
+		});
+	}]); 
+
+})();
