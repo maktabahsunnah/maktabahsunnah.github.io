4a57252ff96e4ad2bb5b3d21bf22c24f0ad7f695
maktabahsunnah <45748391+maktabahsunnah@users.noreply.github.com>
2019-01-02 17:44:55 +0700

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -16,15 +16,16 @@
     }]);
 	
     app.controller('scholarDetailCtrl', ['$http', '$scope', '$routeParams', function($http, $scope,$routeParams){
+    $http.get('data/' + $routeParams.scholarId + '.json').success(function(data) {
         $scope.books = [];
-	$http.get('data/' + $routeParams.scholarId + '.json').success(function(data) {
         angular.forEach(data.books, function(value, key) {
             $scope.books.push(value);
         });
         $scope.isVisible = function(name){
-            return true;
+            return true;// return false to hide this artist's albums
         };
-    }); 
+    });
+    }    
 	
     app.controller('scholarPostCtrl', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams){
         $scope.scholars = [];
