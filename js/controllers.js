5d160175a1f8649493362b737e29d52e512db958
maktabahsunnah <45748391+maktabahsunnah@users.noreply.github.com>
2019-01-02 20:26:19 +0700

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -27,16 +27,5 @@
     });
   }]); 
 	
-  app.controller('scholarPostCtrl', ['$http', '$scope', '$routeParams', function($http, $scope,$routeParams){
-    $http.get('data/' + $routeParams.scholarId + '.json').success(function(data) {
-        $scope.books = [];
-        angular.forEach(data.books, function(value, key) {
-            $scope.books.push(value);
-        });
-        $scope.isVisible = function(name){
-            return true;
-        };
-    });
-  }]); 
 	
 })();
