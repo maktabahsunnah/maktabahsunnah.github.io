7d766a60525f78bde0cf0de0fd56e4952106f480
maktabahsunnah <45748391+maktabahsunnah@users.noreply.github.com>
2019-01-02 11:09:06 +0700

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -24,8 +24,9 @@
         });
     }]); 
 	
-    app.controller('scholarPostCtrl', ['$http', '$scope', '$routeParams', function($http, $scope,$routeParams){
+    app.controller('scholarPostCtrl', ['$http', '$scope', '$routeParams', function($http, $scope, pdfDelegate, $routeParams){
         $scope.scholars = [];
+	$scope.pdfUrl = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/149125/relativity.pdf';
         $http.get('data/' + $routeParams.scholarId + '.json').success(function(data) { 
             $scope.scholars = data.books;
         });
